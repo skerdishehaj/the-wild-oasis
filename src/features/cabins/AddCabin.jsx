1ee438fa6e312d1859653e297879084eee4badbe
Skerdi Shehaj <skerdianshehaj@gmail.com>
2023-11-11 01:11:18 +0100

    Converting the modal to a Compaund Component Pattern

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,9 +1,28 @@
-import { useState } from 'react';
 import CreateCabinForm from './CreateCabinForm';
 import Button from '../../ui/Button';
 import Modal from '../../ui/Modal';
+import CabinTable from './CabinTable';
 
 function AddCabin() {
+  return (
+    <Modal>
+      <Modal.Open opens='cabin-form'>
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name='cabin-form'>
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens='table'>
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name='table'>
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
+  );
+}
+/* function AddCabin() {
   const [isOpenModal, setIsOpenModal] = useState(false);
 
   return (
@@ -18,6 +37,6 @@ function AddCabin() {
       )}
     </div>
   );
-}
+} */
 
 export default AddCabin;
