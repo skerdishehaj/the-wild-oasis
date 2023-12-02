c452372ff3c464eb12972bb97276e1b94751273d
Skerdi Shehaj <skerdianshehaj@gmail.com>
2023-12-03 00:07:44 +0100

    Added the logout button to the header and implemented the logout functionality (logout service and custom hook)

---
+++ src/features/authentication/useLogout.js
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { logout as logouttApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+export function useLogout() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutate: logout, isLoading } = useMutation({
+    mutationFn: logouttApi,
+    onSuccess: () => {
+      queryClient.removeQueries();
+      navigate('/login', { replace: true });
+    },
+    onError: (error) => {
+      console.log('Error', error);
+      toast.error('An error ocurred when trying to logout');
+    },
+  });
+  return { logout, isLoading };
+}
