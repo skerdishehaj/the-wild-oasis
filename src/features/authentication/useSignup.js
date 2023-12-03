bda86cf8d44c7c33905d8587f7984324cdb03bcc
Skerdi Shehaj <skerdianshehaj@gmail.com>
2023-12-03 01:10:54 +0100

    Implemented the signup service and custom hook, with email verification from supabase

---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,20 @@
+import { useMutation } from '@tanstack/react-query';
+import { signup as signupApi } from '../../services/apiAuth.js';
+import toast from 'react-hot-toast';
+
+export function useSignup() {
+  const { mutate: signup, isLoading } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: (data) => {
+      console.log(data);
+      toast.success(
+        "User signed up successfully! Please verify the new account form the user's email address.",
+      );
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error(error.message);
+    },
+  });
+  return { signup, isLoading };
+}
