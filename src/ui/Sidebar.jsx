7b29f40a7eddb801f17325da7adb75e7f9fc0f4c
Skerdi Shehaj <skerdianshehaj@gmail.com>
2023-12-03 22:49:46 +0100

    Fixed typo

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,7 +3,7 @@ import Logo from './Logo';
 import MainNav from './MainNav';
 // import Uploader from '../data/Uploader';
 
-const StyledSidebar = styled.aside`
+const StyledSideBar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
@@ -13,14 +13,14 @@ const StyledSidebar = styled.aside`
   gap: 3.2rem;
 `;
 
-function Sidebar() {
+function SideBar() {
   return (
-    <StyledSidebar>
+    <StyledSideBar>
       <Logo />
       <MainNav />
       {/* <Uploader /> */}
-    </StyledSidebar>
+    </StyledSideBar>
   );
 }
 
-export default Sidebar;
+export default SideBar;
